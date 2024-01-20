b6f6232097f31ca1f4c3e054dcae44d3fc7d09ff
Ali Akbari <7731.aa@gmail.com>
2024-01-20 15:31:35 +0330

    use a status to store the request state

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,10 +2,6 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
 import {
   PokemonForm,
   fetchPokemon,
@@ -14,25 +10,34 @@ import {
 } from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
   const [pokemon, setPokemon] = React.useState(null)
   const [error, setError] = React.useState(null)
+  const [state, setState] = React.useState('idle')
 
   React.useEffect(() => {
-    console.log('rerendering...')
     if (!pokemonName) return
     setPokemon(null)
+    setState('pending')
     fetchPokemon(pokemonName)
       .then(data => {
         console.log(data)
         setPokemon(data)
+        setState('resolved')
       })
-      .catch(err => setError(err.message))
+      .catch(err => {
+        setError(err.message)
+        setState('rejected')
+      })
   }, [pokemonName])
 
   return (
     <>
-      {error && <div><p>There was an error: </p>{error}</div>}
+      {error && (
+        <div>
+          <p>There was an error: </p>
+          {error}
+        </div>
+      )}
       {!pokemonName && 'Submit a pokemon'}
       {!error &&
         pokemonName &&
