8ef94a9f5bf4e097633390bab305acb36d09f29d
Ali Akbari <7731.aa@gmail.com>
2024-01-27 11:20:39 +0330

    store the states in an object

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,10 +2,6 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
 import {
   PokemonForm,
   PokemonInfoFallback,
@@ -14,22 +10,24 @@ import {
 } from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  const [status, setStatus] = React.useState('idle')
-  const [pokemon, setPokemon] = React.useState(null)
-  const [error, setError] = React.useState(null)
+  const [state, setState] = React.useState({
+    status: 'idle',
+    pokemon: null,
+    error: null,
+  })
+
+  const {status, pokemon, error} = state
 
   React.useEffect(() => {
     if (!pokemonName) return
 
-    setStatus('pending')
+    setState({status: 'pending'})
     fetchPokemon(pokemonName).then(
-      pokemonData => {
-        setPokemon(pokemonData)
-        setStatus('resolved')
+      pokemon => {
+        setState({pokemon, status: 'resolved'})
       },
       error => {
-        setError(error)
-        setStatus("rejected")
+        setState({error, status: 'rejected'})
       },
     )
   }, [pokemonName])
